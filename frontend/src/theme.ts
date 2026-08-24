e1b523fb6c9ed854bed2090f15d60cf73261064c
Heitor Guerra <heitor.guerra@proton.me>
2026-08-24 18:10:05 -0300

    Init project

---
+++ frontend/src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#15803d",
+      light: "#22c55e",
+      dark: "#166534",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#f97316",
+      light: "#fb923c",
+      dark: "#c2410c",
+      contrastText: "#ffffff",
+    },
+  },
+});
